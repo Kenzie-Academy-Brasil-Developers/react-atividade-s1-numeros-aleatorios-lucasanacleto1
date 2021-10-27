f8472b0efe3a66f7f8aa821bedb7055557975599
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-10-27 15:30:09 -0300

    finishing random number generator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Numero from "./components/RandomNumber";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Numero />
+      </header>
+    </div>
+  );
+}
+
+export default App;
